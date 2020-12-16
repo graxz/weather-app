d1b5366677f61bac54e513cd6a2cc10f8878f885
Isaddora <isaddorafreitasar@gmail.com>
2020-12-16 10:14:30 -0300

    Everything's OK!

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+console.log('Foi carregado!')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const location = search.value
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            messageOne.textContent = data.error
+            messageTwo.textContent = ''
+        } else {
+            messageOne.textContent = `In ${data.location} `
+            messageTwo.textContent = data.forecast
+        }
+    })
+})
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+// Kelvin: units = s;
+// Fahrenheit: units = f;
+
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=ea5f807feac2f6583ffc3b262d325dba&query=${longitude},${latitude}`
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Conecte-se em uma rede de internet!')
+        } else if (body.error) {
+            callback('Coordenadas erradas!')
+        } else {
+            callback(undefined, 
+                `Now the weather is ${body.current.weather_descriptions[0]} with a temperature of ${body.current.temperature} degrees and a feeling of ${body.current.feelslike} degrees :)`)
+        }
+    })
+}
+
+module.exports = forecast
